dfda74ab0a1e3498d55e8e80d94c9a7d9e2fe9a1
Zachary Truckel <zachary.truckel@zacharytruckelgames.ca>
2016-05-06 16:38:39 -0400

    gitignore

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
     public float speed = 3.0f;
+    public float timer = 5.0f;
 
     private float velocity;
 	// Use this for initialization
@@ -11,7 +12,14 @@ public class Projectile : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () { 
+	void Update () {
+        timer -= Time.deltaTime;
+
         transform.Translate(0, -velocity, 0);
+
+        if (timer < 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
 	}
 }
